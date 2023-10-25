044d243060f5f6a985a4481031dca2890dcdb729
kali973 <gilles.coutard@outlook.fr>
2023-10-25 11:52:28 +0000

    Initialisation projet

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,19 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+<
+phishing.xml,7/9/79225d44d6e655835bd6e907a3ec51fa215bbb6d
+J
+src/commands/commands.html,5/2/52886df1ee4635c6f27fafc6d3e7910b38719f03
+>
+.eslintrc.json,7/1/71c20a341bfe32aa5cd5b4384e94f942bab6a7d3
+?
+CONTRIBUTING.md,3/f/3f454a98e586d1aa0d322e19afd5e67e08f2d3c8
+I
+src/taskpane/taskpane.css,6/3/63ebee315b94e3484e02d803830ecae9e1337696
+J
+src/taskpane/taskpane.html,9/f/9f4233f7de86489a04d68a2541e1e865cc71787f
+H
+src/taskpane/taskpane.js,0/3/03e9956176e2590af19f504eeec4ad4f4a317246
+H
+src/commands/commands.js,9/0/90b388f85746b2a7df574d0ab0cea7a095ccf403
---
+++ src/commands/commands.js
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+/* global global, Office, self, window */
+Office.onReady(() => {
+    // If needed, Office.js is ready to be called
+});
+
+function getGlobal() {
+    return typeof self !== "undefined"
+        ? self
+        : typeof window !== "undefined"
+            ? window
+            : typeof global !== "undefined"
+                ? global
+                : undefined;
+}
+
+const g = getGlobal();
+
+function sucessNotif(msg) {
+    var id = "0";
+    var details = {
+        type: "informationalMessage",
+        icon: "Icon.16x16",
+        message: msg,
+        persistent: false
+    };
+    Office.context.mailbox.item.notificationMessages.addAsync(id, details, function (value) {
+    });
+}
+
+function failedNotif(msg) {
+    var id = "0";
+    var details = {
+        type: "informationalMessage",
+        icon: "Icon.16x16",
+        message: msg,
+        persistent: false
+    };
+    Office.context.mailbox.item.notificationMessages.addAsync(id, details, function (value) {
+    });
+}
+
+function getItemRestId() {
+    if (Office.context.mailbox.diagnostics.hostName === "OutlookIOS") {
+        // itemId is already REST-formatted.
+        return Office.context.mailbox.item.itemId;
+    } else {
+        // Convert to an item ID for API v2.0.
+        return Office.context.mailbox.convertToRestId(
+            Office.context.mailbox.item.itemId,
+            Office.MailboxEnums.RestVersion.v2_0
+        );
+    }
+}
+
+/* Simple Forward */
+function simpleForwardEmail() {
+    Office.context.mailbox.getCallbackTokenAsync({isRest: true}, function (result) {
+        var ewsId = Office.context.mailbox.item.itemId;
+        var accessToken = result.value;
+        simpleForwardFunc(accessToken);
+    });
+}
+
+function simpleForwardFunc(accessToken) {
+    var itemId = getItemRestId();
+
+    // Construct the REST URL to the current item.
+    // Details for formatting the URL can be found at
+    // https://docs.microsoft.com/previous-versions/office/office-365-api/api/version-2.0/mail-rest-operations#get-messages.
+    var forwardUrl = Office.context.mailbox.restUrl + "/v1.0/me/messages/" + itemId + "/forward";
+
+    const forwardMeta = JSON.stringify({
+        Comment: "FYI",
+        ToRecipients: [
+            {
+                EmailAddress: {
+                    Name: "ccmbercy",
+                    Address: "gco-ccm@outlook.fr"
+                }
+            }
+        ]
+    });
+
+    $.ajax({
+        url: forwardUrl,
+        type: "POST",
+        dataType: "json",
+        contentType: "application/json",
+        data: forwardMeta,
+        headers: {Authorization: "Bearer " + accessToken}
+    }).always(function (response) {
+        sucessNotif("Transfert d'e-mail réussi !");
+    });
+}
+
+
+/* Forward as Attachment */
+function forwardAsAttachment() {
+    Office.context.mailbox.getCallbackTokenAsync({isRest: true}, function (result) {
+        var ewsId = Office.context.mailbox.item.itemId;
+        var accessToken = result.value;
+        forwardAsAttachmentFunc(accessToken);
+    });
+}
+
+function forwardAsAttachmentFunc(accessToken) {
+    var itemId = getItemRestId();
+    var getAnItemUrl = Office.context.mailbox.restUrl + "/v1.0/me/messages/" + itemId;
+    var sendItemUrl = Office.context.mailbox.restUrl + "/v1.0/me/sendmail";
+
+    $.ajax({
+        url: getAnItemUrl,
+        type: "GET",
+        contentType: "application/json",
+        headers: {Authorization: "Bearer " + accessToken}
+    }).done(function (responseItem) {
+        // #microsoft.graph.message
+        // microsoft.graph.outlookItem
+        responseItem['@odata.type'] = "#microsoft.graph.message";
+
+        /* Now send mail */
+        const sendMeta = JSON.stringify({
+            "Message": {
+                "Subject": "Veuillez vérifier les activités de hameçonnage !",
+                "Body": {
+                    "ContentType": "Text",
+                    "Content": "Veuillez vérifier les activités de hameçonnage et faites-nous savoir !"
+                },
+                "ToRecipients": [{
+                    "EmailAddress": {
+                        "Address": "gco-cco@outlook.fr"
+                    }
+                }],
+                "Attachments": [
+                    {
+                        "@odata.type": "#Microsoft.OutlookServices.ItemAttachment",
+                        // #Microsoft.OutlookServices.ItemAttachment - worked with graph explorer
+                        // #Microsoft.graph.ItemAttachment - from stack overfloow
+                        "Name": responseItem.Subject,
+                        "Item": responseItem
+                    }
+                ]
+            },
+            "SaveToSentItems": "false"
+        }); // Json.stringify ends
+
+        $.ajax({
+            url: sendItemUrl,
+            type: "POST",
+            dataType: "json",
+            contentType: "application/json",
+            data: sendMeta,
+            headers: {Authorization: "Bearer " + accessToken}
+        }).done(function (response) {
+            sucessNotif("Email forward as attachment successful!");
+        }).fail(function (response) {
+            failedNotif(response);
+        }); // ajax of send mail ends
+
+    }); // ajax.get.done ends
+}
